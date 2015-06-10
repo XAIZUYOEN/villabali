e95357642f0d262a858c48bc00c13e0d56d23b67
Gunawan Wijaya <gunawanwijayaa@gmail.com>
2015-06-10 09:37:58 +0700

    sketching + cdn
    all of library already serve in bower

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # villabali
-test from www.villa-bali.com
+test from http://www.villa-bali.com
+
+1. run `bower install` on root
+
+2. navigate to purecss `cd bower_components/purecss`
+
+3. do `npm install`
+
+4. then `grunt release`
---
+++ assets/script.js
@@ -0,0 +1,14 @@
+new Pikaday({
+    field: document.getElementById('datepicker')
+});
+new Swiper('.swiper-container', {
+    // direction: 'vertical',
+    loop: true,
+    parallax: true,
+    grabCursor: true,
+    paginationClickable: true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    // scrollbar: '.swiper-scrollbar',
+});
