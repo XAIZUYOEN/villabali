89b5b22a43a016d7194b0d5bf49adfade2e8c749
Gunawan Wijaya <gunawanwijayaa@gmail.com>
2015-06-11 05:24:55 +0700

    new image slider + pikaday
    Add image for slider
    Add pikaday compat

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,35 @@
+var tmp;
 new Pikaday({
-    field: document.getElementById('datepicker')
+    format: 'YYYY/MM/DD',
+    minDate: moment().toDate(),
+    maxDate: moment().add(1, 'years').toDate(),
+    yearRange: 1,
+    field: document.getElementById('checkin')
+});
+new Pikaday({
+    format: 'YYYY/MM/DD',
+    minDate: moment().toDate(),
+    maxDate: moment().add(1, 'years').toDate(),
+    yearRange: 1,
+    field: document.getElementById('checkout')
 });
 new Swiper('.swiper-container', {
-    // direction: 'vertical',
     loop: true,
-    parallax: true,
-    grabCursor: true,
-    paginationClickable: true,
+    effect: 'fade',
     pagination: '.swiper-pagination',
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
+    paginationClickable: true,
+    autoplay: 3000,
+    autoplayDisableOnInteraction: false,
+    // direction: 'vertical',
+    // grabCursor: true,
     // scrollbar: '.swiper-scrollbar',
+});
+tmp = document.getElementById('tobe_header');
+new Waypoint({
+    element: tmp,
+    handler: function() {
+        console.log('sasugaaa');
+    }
 });
